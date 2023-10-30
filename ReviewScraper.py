e194458caac572409b934af0818ef67afc280880
jugurca <98515536+jugurca@users.noreply.github.com>
2023-10-31 02:22:57 +0300

    Update ReviewScraper.py

--- ReviewScraper.py
+++ ReviewScraper.py
@@ -1,18 +1,11 @@
 import streamlit as st
-import os
-import sys
-
-@st.cache_resource
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 import time
 import pandas as pd
 from user_agent import generate_user_agent
@@ -52,7 +45,7 @@ with col3:
 
 
 user_agent = generate_user_agent()
-options = FirefoxOptions()
+options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 options.add_argument('--start-maximized')
@@ -60,6 +53,11 @@ options.add_argument('--window-size=1920,1080')
 options.add_argument("--no-sandbox")
 options.add_argument("--incognito")
 options.add_argument(f"user-agent={user_agent}")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
 
 
 st.title("Trendyol Review Scraper")
@@ -82,8 +80,7 @@ progress_bar=st.progress(0)
 
 if st.button("Get the Data"):
     with st.spinner("The process is ongoing..."):
-        if url:
-            driver = webdriver.Firefox(options=options)        
+        if url:       
             wait= WebDriverWait(driver,15)
             feedback_data = []
             try:
