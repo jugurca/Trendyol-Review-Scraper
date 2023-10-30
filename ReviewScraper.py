9a3f9c346d172452dd937a67c7a375e939694213
jugurca <98515536+jugurca@users.noreply.github.com>
2023-10-31 01:53:32 +0300

    Update ReviewScraper.py

--- ReviewScraper.py
+++ ReviewScraper.py
@@ -5,7 +5,7 @@ import sys
 @st.cache_resource
 def installff():
   os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
 _ = installff()
 from selenium import webdriver
